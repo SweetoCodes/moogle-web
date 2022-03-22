c6cce01afd7fc486e7a8cf169394f1a507403cd6
SweetoCodes <charliesweeting18@gmail.com>
2022-03-22 19:43:47 +0000

    added search bar figures

--- components/molecules/search-form.tsx
+++ components/molecules/search-form.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import { useTranslate, Search } from "../../utils/helpers/functions";
+import MagnifyingGlass from "../atoms/icons/magnifying-glass";
+import Microphone from "../atoms/icons/microphone";
 
 interface Props {
   style: string;
+  
 }
 const SearchInput: React.FC<Props> = (style) => {
   const [translation, getTranslation] = useTranslate("");
 
   return (
-    <div className={" mx-auto " + style}>
-      <div className="flex flex-col">
-        <div className=" w-[272px]  ">
+    <div className={" mx-auto w-full " + style}>
+      <div className="flex flex-col w-full ">
+        <div className=" w-[272px] mx-auto ">
           <img src={"/assets/moogle-logo.png"} />
         </div>
 
@@ -22,20 +25,33 @@ const SearchInput: React.FC<Props> = (style) => {
             Search(translation);
           }}
         >
-          <div className="flex flex-row h-[44px] max-w-[484px] border-[1px] rounded-[24px] border-[#dfe1e5]">
-            <div className="flex align-middle pr-[13px]">glass</div>
+          <div className="flex pt-[5px] pr-[8px] pl-[14px] flex-row h-[44px] max-w-[592px] border-[1px] rounded-[24px] border-[#dfe1e5] w-full mx-auto items-center ">
+            <div className="pr-[13px] align-middle ">
+              <MagnifyingGlass style="flex  " />
+            </div>
+            {/* <div>m</div> */}
             <input
-              className="grow"
+              className="grow focus:outline-none"
               type="text"
               value={translation}
               onChange={getTranslation}
             />
-            <div>mic</div>
+            <div className=" px-[8px] align-middle ">
+            <Microphone />
+            </div>
           </div>
 
-          <div className="flex flex-row">
-            <div>button1 </div>
-            <div>button1 </div>
+          <div className="flex flex-row mx-auto space-x-4">
+            <div
+              onClick={(e) => {
+                e.preventDefault();
+                Search(translation);
+              }}
+              className="bg-gray-100"
+            >
+              Moogle Search
+            </div>
+            <div className="bg-gray-100"> I'm Feeling Lucky</div>
           </div>
         </form>
       </div>
